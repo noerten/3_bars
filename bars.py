efe3ad92474fc5d956428a8a98ac19aaf85c79e5
noerten <darkstria@gmail.com>
2016-12-23 23:30:41 +0300

    changes accordint to requirements

--- bars.py
+++ bars.py
@@ -1,50 +1,38 @@
 import json
+import os
 
 def load_data(filepath):
+    if not os.path.exists(filepath):
+        return None
     with open(filepath) as data_file:    
         data = json.load(data_file)
     return data
 
 def get_biggest_bar(data):
-    biggest_seats = 0
-    for i in data:
-        seats = i['SeatsCount']
-        if seats > biggest_seats:
-            biggest_seats = seats
-            biggest_bar = i['Name']
-#    print(biggest_seats)
-    return biggest_bar    
+    biggest_bar_obj = max(data, key=lambda i: i['SeatsCount'])
+    return biggest_bar_obj['Name']    
 
 def get_smallest_bar(data):
-    smallest_seats = float("inf")
-    for i in data:
-        seats = i['SeatsCount']
-        if seats < smallest_seats:
-            smallest_seats = seats
-            smallest_bar = i['Name']
-#    print(smallest_seats)
-    return smallest_bar    
+    smallest_bar_obj = min(data, key=lambda i: i['SeatsCount'])
+    return smallest_bar_obj["Name"]  
 
 def get_closest_bar(data, longitude, latitude):
-    smallest_dist = float("inf")
-    for i in data:
-        bar_longitude = i['geoData']['coordinates'][0]
-        bar_latitude = i['geoData']['coordinates'][1]
-        dist = ((bar_longitude - longitude) ** 2
-                + (bar_latitude - latitude) ** 2) ** (1/2)
-        if dist < smallest_dist:
-            smallest_dist = dist
-            closest_bar = i['Name']
-#    print(dist)
-    return closest_bar
-
+    for bar in data:
+        #bar_coords - list with longitude and latitude
+        bar_coords = bar['geoData']['coordinates']
+        bar['dist_to_user'] = ((bar_coords[0] - longitude) ** 2
+            + (bar_coords[1] - latitude) ** 2) ** (1/2)
+    closest_bar_obj = min(data, key=lambda i: i['dist_to_user'])
+    return closest_bar_obj['Name']
+                          
 if __name__ == '__main__':
-    data = load_data('data.json')
+    file_name = input('Enter file name/path: ')
+    data = load_data(file_name)
     biggest_bar = get_biggest_bar(data)
-    print('biggest bar', biggest_bar)
     smallest_bar = get_smallest_bar(data)
-    print('smallest bar', smallest_bar)
+    print("smallest bar: {}\n"
+          "biggest bar: {}\n".format(smallest_bar, biggest_bar))
     longitude = float(input('input longitude: '))
     latitude = float(input('input latitude: '))
     closest_bar = get_closest_bar(data, longitude, latitude)
-    print('closest bar', closest_bar)
+    print('closest bar:', closest_bar)

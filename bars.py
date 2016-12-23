f81ad2dec7dce84762e7f9962a6ef7aa900f1c5d
noerten <darkstria@gmail.com>
2016-12-24 01:00:54 +0300

    pep8

--- bars.py
+++ bars.py
@@ -1,30 +1,35 @@
 import json
 import os
 
+
 def load_data(filepath):
     if not os.path.exists(filepath):
         return None
-    with open(filepath) as data_file:    
+    with open(filepath) as data_file:
         data = json.load(data_file)
     return data
 
+
 def get_biggest_bar(bars):
     biggest_bar_obj = max(bars, key=lambda i: i['SeatsCount'])
-    return biggest_bar_obj['Name']    
+    return biggest_bar_obj['Name']
+
 
 def get_smallest_bar(bars):
     smallest_bar_obj = min(bars, key=lambda i: i['SeatsCount'])
-    return smallest_bar_obj["Name"]  
+    return smallest_bar_obj["Name"]
+
 
 def get_closest_bar(bars, longitude, latitude):
     for bar in bars:
-        #bar_coords - list with longitude and latitude
+        # bar_coords - list with longitude and latitude
         bar_coords = bar['geoData']['coordinates']
-        bar['dist_to_user'] = ((bar_coords[0] - longitude) ** 2
-            + (bar_coords[1] - latitude) ** 2) ** (1/2)
+        bar['dist_to_user'] = ((bar_coords[0] - longitude) ** 2 +
+                               (bar_coords[1] - latitude) ** 2) ** (1/2)
     closest_bar_obj = min(bars, key=lambda i: i['dist_to_user'])
     return closest_bar_obj['Name']
-                          
+
+
 if __name__ == '__main__':
     file_name = input('Enter file name/path: ')
     bars = load_data(file_name)
